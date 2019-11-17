ac9fe5df89cf20ec907f8e4128f7fe7cbb2ad431
hektormisplon <hektor.misplon@gmail.com>
2019-11-17 02:43:48 +0100

    preact setup

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import { h } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style.css';
+
+const Header = () => (
+	<header class={style.header}>
+		<h1>Three.js experiments</h1>
+		<nav>
+			<Link activeClassName={style.active} href="/example-1">Example 1 - </Link>
+			<Link activeClassName={style.active} href="/example-2">Example 2 - </Link>
+			<Link activeClassName={style.active} href="/example-3">Example 3 - </Link>
+		</nav>
+	</header>
+);
+
+export default Header;
